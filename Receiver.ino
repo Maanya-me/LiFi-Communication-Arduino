bf96870888721f7fa513a37d1063708554d9556b
Maanya-me <maanyasahums@gmail.com>
2026-04-15 23:41:08 +0530

    Update Receiver.ino

--- Receiver.ino
+++ Receiver.ino
@@ -1,22 +1,33 @@
 #define SENSOR A0
 
+char receivedChar;
+
 void setup()
 {
   Serial.begin(9600);
 }
 
+char readByte()
+{
+  char data = 0;
+
+  for(int i = 0; i < 8; i++)
+  {
+    int lightValue = analogRead(SENSOR);
+
+    if(lightValue > 500)
+    {
+      bitSet(data, i); 
+    }
+
+    delay(100); // same as transmitter timing
+  }
+
+  return data;
+}
+
 void loop()
 {
-  int lightValue = analogRead(SENSOR);
-
-  if(lightValue > 500)
-  {
-    Serial.println("LIGHT SIGNAL DETECTED");
-  }
-  else
-  {
-    Serial.println("NO SIGNAL");
-  }
-
-  delay(100);
+  receivedChar = readByte();
+  Serial.print(receivedChar);
 }
